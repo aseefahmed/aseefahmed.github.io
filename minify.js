421ee62f4a29bb6d3af869f90bc3d2684600f3af
Aseef Ahmed <aseefahmed@gmail.com>
2019-09-05 14:53:32 +0600

    4

--- minify.js
+++ minify.js
@@ -18,18 +18,18 @@ minify({
 minify({
   compressor: uglifyjs,
   input: [
-  			'js/jquery-2.1.3.min.js',
-  			'js/modernizr.custom.js',
-  			'js/pages-switcher.js', 
-  			'js/imagesloaded.pkgd.min.js',
-  			'js/validator.js',
-  			'js/jquery.shuffle.min.js',
-  			'js/masonry.pkgd.min.js',
-  			'js/owl.carousel.min.js',
-  			'js/jquery.magnific-popup.min.js',
-  			'js/jquery.hoverdir.js',
-  			'js/main.js'
-  		],
+        'js/jquery-2.1.3.min.js',
+        'js/modernizr.custom.js',
+        'js/pages-switcher.js', 
+        'js/imagesloaded.pkgd.min.js',
+        'js/validator.js',
+        'js/jquery.shuffle.min.js',
+        'js/masonry.pkgd.min.js',
+        'js/owl.carousel.min.js',
+        'js/jquery.magnific-popup.min.js',
+        'js/jquery.hoverdir.js',
+        'js/main.js'
+      ],
   output: 'js/all.min.js',
   callback: function(err, min) {}
 }); 
@@ -38,14 +38,15 @@ minify({
 minify({
   compressor: sqwish,
   input: [
-  			'css/bootstrap.css', 
-  			'css/normalize.css',
-  			'css/animate.css',
-  			'css/transition-animations.css',
-  			'css/owl.carousel.css',
-  			'css/magnific-popup.css',
-  			'css/main-green.css',
-  		],
+        'css/bootstrap.css', 
+        'css/font-awesome.css', 
+        'css/normalize.css',
+        'css/animate.css',
+        'css/transition-animations.css',
+        'css/owl.carousel.css',
+        'css/magnific-popup.css',
+        'css/main-green.css',
+      ],
   output: 'css/all.min.css',
   callback: function(err, min) {}
 });
